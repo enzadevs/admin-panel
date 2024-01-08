3a90dc63f720d8a53acfa5df29d6394e269347ad
enzadevs <e.eziz5@yandex.ru>
2024-01-08 18:22:25 +0500

    Bush

--- app/home/users/page.jsx
+++ app/home/users/page.jsx
@@ -36,7 +36,7 @@ const usersTableHeaders = [
 ]
 
 export default async function UsersPage(){
-    const data = await fetch('')
+    // const data = await fetch('')
 
     return(
         <div className='flex flex-col gap-4 p-4'>
