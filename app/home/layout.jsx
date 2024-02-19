f915db03da8bc309e2b5f0633d249eb6a08e1ba5
Enza <e.eziz5@yandex.ru>
2024-02-20 00:07:17 +0500

    Repush

--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -1,4 +1,9 @@
-import ProtectedRoute from "utils/ProtectedRoute";
+"use client";
+
+import dynamic from "next/dynamic";
+const ProtectedRoute = dynamic(() => import("utils/ProtectedRoute.jsx"), {
+  ssr: false,
+});
 import SideBar from "components/Navigation/SideBar/SideBar";
 
 export default function HomeLayout({ children }) {
