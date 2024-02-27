c7024679854e2a475552539390ea161042c12fdb
Enza <e.eziz5@yandex.ru>
2024-02-27 18:49:05 +0500

    Update page.jsx

--- app/home/manage/categories/[id]/page.jsx
+++ app/home/manage/categories/[id]/page.jsx
@@ -19,11 +19,29 @@ export default function ViewCategoryPage({ params }) {
 
   const createNewSubCategory = async (e) => {
     e.preventDefault();
+
+    const body = {
+      title: subCategoryTitleRef.current.value,
+      id: params.id,
+    };
+
+    const response = await fetch(
+      `http://localhost:5000/manage/subcategory/create`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      }
+    );
+
     try {
       if (response.ok) {
-        SuccessToast({ successText: "Под категория была успешно создана." });
+        SuccessToast({ successText: "Подкатегория успешно создано." });
+        subCategoryTitleRef.current.value = "";
       } else {
-        ErrorToast({ errorText: "Пожалуйста наполните поле." });
+        ErrorToast({ errorText: "Пожалуйста наполните все поля." });
       }
     } catch (error) {
       if (error.response) {
@@ -42,7 +60,7 @@ export default function ViewCategoryPage({ params }) {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-4">
       <h2>{data.title}</h2>
       <div className="flex flex-col gap-4 sm:flex-row">
         <div className="bg-white rounded-lg shadow-md flex flex-col px-2 sm:flex-[50%] pb-4 h-fit">
@@ -65,33 +83,49 @@ export default function ViewCategoryPage({ params }) {
             </button>
           </div>
           <ul className="flex flex-col">
-            {data?.subCategories.map((item) => {
-              return (
-                <li
-                  key={item.id}
-                  className="border-b flex-row-center pl-4 h-10 w-full"
-                >
-                  {item.title}
-                </li>
-              );
-            })}
+            {data.subCategories.length === 0 ? (
+              <p className="bg-yellow-300 border border-yellow-400 rounded-lg center text-xs md:text-sm mt-4 px-4 h-10">
+                В этой категории нет под категорий.
+              </p>
+            ) : (
+              <>
+                {data?.subCategories.map((item) => {
+                  return (
+                    <li
+                      key={item.id}
+                      className="border-b flex-row-center pl-4 h-10 w-full"
+                    >
+                      {item.title}
+                    </li>
+                  );
+                })}
+              </>
+            )}
           </ul>
         </div>
         <div className="bg-white rounded-lg shadow-md flex flex-col px-2 sm:flex-[50%] pb-4 h-fit">
-          <h3 className="flex-row-center font-semibold pl-4 h-10">
-            Список продуктов
+          <h3 className="flex-row-center justify-between font-semibold pl-4 h-10">
+            Список товаров
           </h3>
           <ul className="flex flex-col">
-            {data?.products.map((item) => {
-              return (
-                <li
-                  key={item.id}
-                  className="border-b flex-row-center pl-4 h-10 w-full"
-                >
-                  {item.title}
-                </li>
-              );
-            })}
+            {data.products.length === 0 ? (
+              <p className="bg-yellow-300 border border-yellow-400 rounded-lg center text-xs md:text-sm px-4 h-10">
+                В этой категории нет товаров.
+              </p>
+            ) : (
+              <>
+                {data?.products.map((item) => {
+                  return (
+                    <li key={item.id}>
+                      <div className="border-b flex-row-center justify-between pl-4 h-10 w-full">
+                        <p>{item.title}</p>
+                        <p>{item.sellPrice + " ман."}</p>
+                      </div>
+                    </li>
+                  );
+                })}
+              </>
+            )}
           </ul>
         </div>
       </div>
