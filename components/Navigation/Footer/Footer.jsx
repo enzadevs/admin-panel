39d03b47c5d88060f6af19cc55196d17c7f80b5d
Enza <e.eziz5@yandex.ru>
2024-02-28 19:38:52 +0500

    Update Footer.jsx

--- components/Navigation/Footer/Footer.jsx
+++ components/Navigation/Footer/Footer.jsx
@@ -4,21 +4,22 @@ import AlemTilsimatLogo from "public/assets/alem-tilsimat-logo.webp";
 
 export default function Footer() {
   return (
-    <footer className="bg-calm-50 flex flex-row items-center gap-4 mt-auto px-4 h-24">
+    <footer className="border-t bg-calm-50 flex flex-row items-center gap-4 mt-auto px-4 h-24">
       <span className="text-calm-600 text-lg font-bold flex-row-center">
         <>2024. Admin Panel</>
       </span>
       <Link
         href="https://alemtilsimat.com/#"
-        className="bg-calm-600 rounded-lg flex-row-center ml-auto py-2 px-4"
+        className="bg-calm rounded-lg flex-row-center ml-auto py-2 px-4"
         target="_blank"
       >
         <Image
           src={AlemTilsimatLogo}
           alt="alem tilsimat logo"
           height={30}
-          width={30}
+          width={"auto"}
           sizes="25vw"
+          style={{ objectFit: "contain" }}
         ></Image>
         <p className="text-white text-sm">Älem Tilsimat</p>
       </Link>
